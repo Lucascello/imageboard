8a8eea4b25a5e70f820fd4a9b01cb5a994139de4
Lucas Ebert <lucascello@yahoo.com.br>
2021-12-20 18:44:09 +0100

    setting up repo

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 app.use(express.json());
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
